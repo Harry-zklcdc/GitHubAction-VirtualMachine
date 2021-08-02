cac6f006d7eb8cef77bb4d6213f54f4ac9e56dfc
zklcdc <36770399+zklcdc@users.noreply.github.com>
2021-08-03 00:01:43 +0800

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -1,4 +1,4 @@
-#linux-run.sh LINUX_USER_PASSWORD NGROK_AUTH_TOKEN LINUX_USERNAME LINUX_MACHINE_NAME XRAY_CONFIG FULLCHAIN_CRT PRIVATE_KEY
+#linux-run.sh LINUX_USER_PASSWORD LINUX_USERNAME LINUX_MACHINE_NAME 
 #!/bin/bash
 # /home/runner/.ngrok2/ngrok.yml
 
@@ -8,30 +8,10 @@ echo "$LINUX_USERNAME:$LINUX_USER_PASSWORD" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $LINUX_MACHINE_NAME
 
-if [[ -z "$NPS_AUTH_TOKEN" ]]; then
-  echo "Please set 'NPS_AUTH_TOKEN'"
-  exit 2
-fi
-
-if [[ -z "$NPS_ADDRESS" ]]; then
-  echo "Please set 'NPS_ADDRESS'"
-  exit 3
-fi
-
 if [[ -z "$LINUX_USER_PASSWORD" ]]; then
   echo "Please set 'LINUX_USER_PASSWORD' for user: $USER"
   exit 4
 fi
 
-echo "### Install NPS ###"
-
-wget -q https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz
-tar -xzf linux_amd64_client.tar.gz
-chmod +x ./npc
-
 echo "### Update user: $USER password ###"
 echo -e "$LINUX_USER_PASSWORD\n$LINUX_USER_PASSWORD" | sudo passwd "$USER"
-
-echo "### Start NPS proxy for 22 port ###"
-sudo ./npc install -server="$NPS_ADDRESS" -vkey="$NPS_AUTH_TOKEN" -type=tcp
-sudo npc start
