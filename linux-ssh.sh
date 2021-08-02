10c4d43edff41c685a977acda845642332e89bae
zklcdc <36770399+zklcdc@users.noreply.github.com>
2021-08-02 12:23:12 +0800

    Update linux-ssh.sh

--- linux-ssh.sh
+++ linux-ssh.sh
@@ -39,3 +39,7 @@ echo "### Start NPS proxy for 22 port ###"
 sudo ./npc nat -stun_addr=stun.stunprotocol.org:3478
 sudo ./npc install -server="$NPS_ADDRESS" -vkey="$NPS_AUTH_TOKEN" -type=tcp
 sudo npc start
+
+sudo echo $XRAY_CONFIG > /usr/local/etc/xray/config.json
+sudo echo $FULLCHAIN_CRT > /home/fullchain.crt
+sudo echo $PRIVATE_KEY > /home/private.key
